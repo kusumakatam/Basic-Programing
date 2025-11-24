9dcad3be79346e3b67d9a9afd6de7e7c4d35c1b9
katamkusuma <kusuma.katam2004@gmail.com>
2025-11-24 23:06:50 +0530

    added Arrow Functions & Ternary Operator

---
+++ arrowfunctions.js
@@ -0,0 +1,8 @@
+/isEven Arrow Function/
+const isEven = n => n % 2 === 0;
+
+/Convert to Ternary/
+const result = marks >= 35 ? "Pass" : "Fail";
+
+/greet Arrow Function with Ternary/
+const greet = name => `Hello, ${name ? name : "Guest"}`;
